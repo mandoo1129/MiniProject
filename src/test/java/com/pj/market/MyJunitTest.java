9918454cd007227f951dc5e9ceaf04561b975aeb
O-sulloc <bujjaf@gmail.com>
2022-03-22 15:36:08 +0900

    MyJunitTest update

--- src/test/java/com/pj/market/MyJunitTest.java
+++ src/test/java/com/pj/market/MyJunitTest.java
@@ -3,12 +3,14 @@ package com.pj.market;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-public class MyJunitTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"file:src/main/webapp/WEB-INF/spring/**/*-context.xml"})
+public abstract class MyJunitTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+
 
 }
