e8e92c57de873703906b171e1fad3000e9a0ab51
mandoo1129 <leeminjoo02011@gmail.com>
2022-03-22 15:05:04 +0900

    update

---
+++ src/test/java/com/pj/market/MyJunitTest.java
@@ -0,0 +1,14 @@
+package com.pj.market;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyJunitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
